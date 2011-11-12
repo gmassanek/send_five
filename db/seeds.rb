5e3bf47fc82b46d911dd774d346662339b50290b
Geoff Massanek <gmassanek@gmail.com>
2011-11-12 12:10:53 -0600

    Seeding and finalizing model-only resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+Vendor.destroy_all
+Vendor.create(:name => "Snarfs")
+Vendor.create(:name => "Starbucks")
+Vendor.create(:name => "Motel Bar")
+Vendor.create(:name => "Humane Society", :charity => true)
+Vendor.create(:name => "Jim Dugans", :charity => true)
+
+puts "Created #{Vendor.count} Vendors"
+
+Code.destroy_all
+Code.create(:vendor => Vendor.all.shuffle.first, :code_number => 8)
+Code.create(:vendor => Vendor.all.shuffle.first, :code_number => 7)
+Code.create(:vendor => Vendor.all.shuffle.first, :code_number => 5)
+Code.create(:vendor => Vendor.all.shuffle.first, :code_number => 3)
+Code.create(:vendor => Vendor.all.shuffle.first, :code_number => 9)
+
+puts "Created #{Code.count} Codes"
