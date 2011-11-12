4ae2409798354f35605c3070eb5db0b4e3e7c51c
Geoff Massanek <gmassanek@gmail.com>
2011-11-12 11:26:41 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SendFive::Application.config.secret_token = '6c27495eb80a5716bddde75a696dc70fe58a261ad4ae5d48789b537d2e8f61424487b253b534962c75be374173fed11e2604383596283eab080b4f133256035e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SendFive::Application.config.session_store :cookie_store, key: '_send_five_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SendFive::Application.config.session_store :active_record_store
