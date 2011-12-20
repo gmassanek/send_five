d8fe62b0ab5c4d4300065c243f027397a1b88d55
David Anderson <daveande@gmail.com>
2011-12-20 13:08:29 -0600

    Changed seed file, took out sign in, and changed success view.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,38 +8,44 @@
 #
 
 Vendor.destroy_all
-Vendor.create(:name => "Snarfs")
 Vendor.create(:name => "Starbucks")
-Vendor.create(:name => "Motel Bar")
-Vendor.create(:name => "Humane Society", :charity => true)
-Vendor.create(:name => "Red Cross", :charity => true)
-
-puts "Created #{Vendor.count} Vendors"
 
 Code.destroy_all
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '834-AB34333')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '7234 3211 0988 2343')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '5A-2343009')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '3FC221-88')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9FC2 21F2 2343 9922')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '2349FT 2343')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '923423XX2')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '92342 123S')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9987 2343 1234 3242')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9FC2 21F2 2343 9922')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9FC2 21F2 2343 9922')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9FC2 21F2 2343 9922')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '2349FT 2343')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '923423XX2')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '92342 123S')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9987 2343 1234 3242')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '2349FT 2343')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '923423XX2')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '92342 123S')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9987 2343 1234 3242')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '2349FT 2343')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '923423XX2')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '92342 123S')
-Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9987 2343 1234 3242')
+Code.create(:vendor => Vendor.find_by_name("Starbucks"), :code_number => '82391-12345')
 
-puts "Created #{Code.count} Codes"
+#  Vendor.destroy_all
+#  Vendor.create(:name => "Snarfs")
+#  Vendor.create(:name => "Starbucks")
+#  Vendor.create(:name => "Motel Bar")
+#  Vendor.create(:name => "Humane Society", :charity => true)
+#  Vendor.create(:name => "Red Cross", :charity => true)
+
+#  puts "Created #{Vendor.count} Vendors"
+#
+#  Code.destroy_all
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '834-AB34333')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '7234 3211 0988 2343')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '5A-2343009')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '3FC221-88')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9FC2 21F2 2343 9922')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '2349FT 2343')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '923423XX2')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '92342 123S')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9987 2343 1234 3242')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9FC2 21F2 2343 9922')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9FC2 21F2 2343 9922')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9FC2 21F2 2343 9922')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '2349FT 2343')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '923423XX2')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '92342 123S')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9987 2343 1234 3242')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '2349FT 2343')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '923423XX2')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '92342 123S')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9987 2343 1234 3242')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '2349FT 2343')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '923423XX2')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '92342 123S')
+#  Code.create(:vendor => Vendor.all.shuffle.first, :code_number => '9987 2343 1234 3242')
+
+#  puts "Created #{Code.count} Codes"
